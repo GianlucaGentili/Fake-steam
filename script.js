066c7db19f4f922646cdce0a4a8992cfb994cdbb
GianlucaGentili <gentiligian@gmail.com>
2024-02-03 17:38:16 +0100

    fixec carousel and footer, added button to return on top of the page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Return on top button
+
+let returnButton = document.getElementById("btnontop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    returnButton.style.display = "block";
+  } else {
+    returnButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 function PlaySound(soundobj) {
     var thissound = document.getElementById(soundobj);
     thissound.play();
@@ -8,3 +29,4 @@ function PlaySound(soundobj) {
     thissound.pause();
     thissound.currentTime = 0;
   }
+  
